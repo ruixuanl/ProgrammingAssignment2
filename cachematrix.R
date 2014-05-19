8ccd00b6cb9fd7c2d89a9c8df14c45d0fb6948a7
ruixuanl <rxli_08@yahoo.com>
2014-05-19 02:08:32 -0400

    r
    HWK

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## [Put comments here that describe what your functions do]
+install.packages("MASS")
+library(MASS)
 makeCacheMatrix <- function(x = matrix()) {
-
+    library(MASS)
+    m<-NULL
+    set<-function(y) {
+        x<<- y
+        m<<- NULL
+    }
+    get<-function() x
+    setInv<- function (ginv) m <<- ginv
+    getInv<- function() m
+    list(set=set,get=get,setInv=setInv,getInv=getInv)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m<- x$getInv()
+    if (!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data<- x$get()
+    library(MASS)
+    m<- ginv(data, ...)
+    x$setInv(m)
+    m
+       
 }
